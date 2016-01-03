d450864021c3b4a24e27da12686037247aa40f63
Andrei Scripniciuc <scripni@users.noreply.github.com>
2016-01-03 21:38:27 +0000

    Converted session to class

--- src/session.js
+++ src/session.js
@@ -1,44 +1,39 @@
 'use strict';
 
 var net = require('net');
+var assert = require('assert');
 var log = require('./logger');
 var ficsHost = process.env.FICS_HOST || 'freechess.org';
 var ficsPort = process.env.FICS_PORT || 5000;
 
-var Session = function() {
-  var session = {};
-  var client = new net.Socket();
-  // opens a connection to the fics server
-  session.connect = function(done) {
-    var hasCallback = typeof done === 'function';
-    if (!hasCallback) {
-      log.warn('No callback specified calling connect');
-    }
+class Session {
+  constructor() {
+    this.client = new net.Socket();
+  }
 
-    client.connect(ficsPort, ficsHost, function() {
+  // connects to FICS
+  connect(done) {
+    assert.equal(typeof done, 'function');
+    this.client.connect(ficsPort, ficsHost, function() {
       log.info('client connected to ' + ficsHost + ':' + ficsPort);
-      if (hasCallback) {
         done();
-      }
     });
 
-    client.on('data', function(data) {
+    this.client.on('data', function(data) {
       log.info('client sent ' + data);
     });
 
-    client.on('close', function() {
+    this.client.on('close', function() {
       log.info('client closed');
     });
-  };
+  }
 
-  session.signIn = function(done) {
+  signIn(done) {
     log.info('signing in as guest');
     // 2 '\n' characters is enough when using telnet freechess.org:5000,
     // 10 seem to be required with this client... need to figure out why
-    client.write('g\n\n\n\n\n\n\n\n\n\n', 'ASCII', done);
-  };
-
-  return session;
-};
+    this.client.write('g\n\n\n\n\n\n\n\n\n\n', 'ASCII', done);
+  }
+}
 
 module.exports = Session;
