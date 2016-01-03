ff605d434b08dbb30133ef5954b9e4810370c659
Andrei Scripniciuc <scripni@users.noreply.github.com>
2016-01-03 12:18:31 +0000

    Added pending tests

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Node.js client connecting to the Free Internet Chess Server (FICS), freechess.or
     npm install
     gulp serve
 
+This will run code analysis, tests and start a new app instance through nodemon. Editing any file will trigger validation and restart the process.
+
 ## Notes
 
 The connection is made over telnet. From a terminal, run `telnet freechess.org 5000` to manually connect.
--- test/spec/session.spec.js
+++ test/spec/session.spec.js
@@ -9,22 +9,29 @@ mocks.enable();
 
 var Session = require('../../src/session');
 
-describe('a valid session', function() {
-  var session = {};
-  before(function() {
-    session = new Session();
-  });
-  after(function() {
-    mocks.disable();
-  });
-  it('connects to the correct host and signs in', function(done) {
-    session.connect(function() {
-      assert.equal(netMockState.connectArgs[0].port, 5000);
-      assert.equal(netMockState.connectArgs[0].host, 'freechess.org');
-      session.signIn(function() {
-        assert.equal(netMockState.writeArgs[0], 'g\n\n\n\n\n\n\n\n\n\n');
-        done();
+describe('session', function() {
+  describe('valid session', function() {
+    var session = {};
+    before(function() {
+      session = new Session();
+    });
+    after(function() {
+      mocks.disable();
+    });
+    it('connects to the correct host and signs in', function(done) {
+      session.connect(function() {
+        assert.equal(netMockState.connectArgs[0].port, 5000);
+        assert.equal(netMockState.connectArgs[0].host, 'freechess.org');
+        session.signIn(function() {
+          assert.equal(netMockState.writeArgs[0], 'g\n\n\n\n\n\n\n\n\n\n');
+          done();
+        });
       });
     });
+    it('passes received messages to the interpreter');
+  });
+
+  describe('disconnected session', function() {
+    it('fails to sign in');
   });
 });
