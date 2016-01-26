cd3b0b8f2944fa30dab017746d9fc5db9a5fa2c3
Andrei Scripniciuc <scripni@users.noreply.github.com>
2016-01-26 21:04:19 +0000

    added tests for multiple messages in same chunk

--- test/spec/parser.spec.js
+++ test/spec/parser.spec.js
@@ -6,14 +6,12 @@ var mockery = require('mockery');
 var sinon   = require('sinon');
 
 describe('parser', () => {
-  var Parser;
-    var interpreterMock;
+  var Parser, interpreterMock;
 
   before(() => {
     mockery.registerAllowables([
       '../../src/parser',
-      'assert',
-      './logger'
+      'assert'
       ]);
     mockery.registerMock('./interpreter', function() {
       return interpreterMock;
@@ -21,29 +19,50 @@ describe('parser', () => {
     mockery.registerMock('./logger', {
       info: sinon.stub()
     });
-    interpreterMock = {
-      interpret: sinon.spy()
-    };
     mockery.enable();
-
     Parser  = require('../../src/parser');
   });
 
-  describe('breaking down a message based on separator', () => {
-    it('identifies a single message if passed in one chunk', () => {
-      var parser = new Parser('%x');
-      parser.parse('message%x');
-      expect(interpreterMock.interpret.calledWith('message')).to.be.true;
-    });
-    it('identifies a single message if passed in two chunks');
-    it('identifies multiple messages in the same chunk');
-    it('identifies multiple messages in multiple chunks');
+  beforeEach(() => {
+    interpreterMock = {
+      interpret: sinon.spy()
+    };
   });
-
 
   after(function() {
     mockery.disable();
     mockery.deregisterAll();
   });
 
+  describe('breaking down a message based on separator', () => {
+    it('handles a message with no separator', () => {
+      var parser = new Parser('%');
+      parser.parse('message');
+      expect(interpreterMock.interpret.called).to.be.false;
+    });
+
+    it('identifies a single message if passed in one chunk', () => {
+      var parser = new Parser('%');
+      parser.parse('message%');
+      expect(interpreterMock.interpret.calledWith('message')).to.be.true;
+    });
+
+    it('identifies a single message if passed in two chunks', () => {
+      var parser = new Parser('%');
+      parser.parse('part 1');
+      expect(interpreterMock.interpret.called).to.be.false;
+      parser.parse(' part 2%');
+      expect(interpreterMock.interpret.calledWith('part 1 part 2')).to.be.true;
+    });
+
+    it('identifies multiple messages in the same chunk', () => {
+      var parser = new Parser('%');
+      parser.parse('msg1%msg2%msg3');
+      expect(interpreterMock.interpret.calledTwice).to.be.true;
+      expect(interpreterMock.interpret.firstCall.calledWith('msg1')).to.be.true;
+      expect(interpreterMock.interpret.secondCall.calledWith('msg2')).to.be.true;
+    });
+    
+    it('identifies multiple messages in multiple chunks');
+  });
 });
